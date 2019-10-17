39ec4e9d1582de1a7442568fe89bd2b8bbdb0b4f
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-10-17 12:33:43 -0400

    Update SleepingQueens.cs

--- Assets/SleepingQueens.cs
+++ Assets/SleepingQueens.cs
@@ -7,10 +7,18 @@ public class SleepingQueens : MonoBehaviour
     [Header("Set in Inspector")]
     public TextAsset deckXML;
 
+    public GameObject queensCardPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject testCard = Instantiate(queensCardPrefab);
+
+        QueensCard cardData = testCard.GetComponent<QueensCard>();
+
+
+        // cardData.setType (xmlParser.getData[however, that's done].
+
     }
 
     // Update is called once per frame
